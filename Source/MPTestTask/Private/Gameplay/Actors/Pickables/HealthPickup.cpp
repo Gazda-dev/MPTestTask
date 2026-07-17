aece8808cbae4cb64e844e10e8f0b927744504ec
gazda-dev <karimzheadshot@gmail.com>
2026-07-17 19:38:26 +0200

    healthPickup

---
+++ Source/MPTestTask/Private/Gameplay/Actors/Pickables/HealthPickup.cpp
@@ -0,0 +1,65 @@
+﻿// Copyright © 2026 Gazda-dev. All Rights Reserved.
+
+#include "Gameplay/Actors/Pickables/HealthPickup.h"
+
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/Scene.h"
+#include "Gameplay/Components/HealthComponent.h"
+
+AHealthPickup::AHealthPickup()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	bReplicates = true;
+	
+	RootSceneComponent = CreateDefaultSubobject<USceneComponent>("RootSceneComponent");
+	RootComponent = RootSceneComponent;
+	
+	CollisionSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollisionComponent"));
+	CollisionSphereComponent->SetupAttachment(RootSceneComponent);
+	CollisionSphereComponent->InitSphereRadius(50.f);
+	CollisionSphereComponent->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(RootComponent);
+}
+
+bool AHealthPickup::CanInteract(AActor* Interactor) const
+{
+	if (!IsValid(Interactor) || bConsumed)
+	{
+		return false;
+	}
+	
+	if (const UHealthComponent* HealthComponent = Interactor->FindComponentByClass<UHealthComponent>())
+	{
+		return HealthComponent->GetHealth() < HealthComponent->GetMaxHealth();
+	}
+	
+	return false;
+}
+
+void AHealthPickup::Interact(AActor* Interactor)
+{
+	if (!IsValid(Interactor) || bConsumed)
+	{
+		return;
+	}
+	//TODO delete but maybe some proper logs on cvar...
+	UE_LOG(LogTemp, Warning, TEXT("healthpickup interact"));
+	
+	if (UHealthComponent* HealthComponent = Interactor->FindComponentByClass<UHealthComponent>())
+	{
+		HealthComponent->Heal(HealAmount);
+		bConsumed = true;
+		
+		Destroy();
+	}
+}
+
+FText AHealthPickup::GetInteractionPrompt() const
+{
+	return InteractionPrompt;
+}
+
---
+++ Source/MPTestTask/Public/Gameplay/Actors/Pickables/HealthPickup.h
@@ -0,0 +1,44 @@
+﻿// Copyright © 2026 Gazda-dev. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Gameplay/Interfaces/InteractableInterface.h"
+
+#include "HealthPickup.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class MPTESTTASK_API AHealthPickup : public AActor, public IInteractableInterface
+{
+	GENERATED_BODY()
+
+public:
+	AHealthPickup();
+
+	/** IInteractableInterface Interface */
+	virtual bool CanInteract(AActor* Interactor) const override;
+	virtual void Interact(AActor* Interactor) override;
+	virtual FText GetInteractionPrompt() const override;
+	/** End IInteractableInterface Interface */
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "HealthPickup")
+	TObjectPtr<USceneComponent> RootSceneComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "HealthPickup")
+	TObjectPtr<USphereComponent> CollisionSphereComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "HealthPickup")
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "HealthPickup")
+	float HealAmount = 20.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "HealthPickup")
+	FText InteractionPrompt = FText::FromString(TEXT("Pickup medkit"));
+	
+	bool bConsumed = false;
+};
