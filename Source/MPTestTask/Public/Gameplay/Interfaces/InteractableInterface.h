a7b9342bddbbc48df56b5a97567dfb0c1332c926
gazda-dev <karimzheadshot@gmail.com>
2026-07-17 18:06:03 +0200

    interactable interface

---
+++ Source/MPTestTask/Public/Gameplay/Interfaces/InteractableInterface.h
@@ -0,0 +1,32 @@
+﻿// Copyright © 2026 Gazda-dev. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+
+#include "InteractableInterface.generated.h"
+
+UINTERFACE()
+class UInteractableInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class MPTESTTASK_API IInteractableInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool CanInteract(AActor* Interactor) const
+	{
+		return true;
+	}
+	
+	virtual void Interact(AActor* Interactor) = 0;
+	
+	virtual FText GetInteractionPrompt() const
+	{
+		return FText::GetEmpty();
+	}
+};
